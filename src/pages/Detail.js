e5f905e5822def1b49454f5f21973cf97db381bc
DanuDan <ardanuaryhfz@gmail.com>
2022-10-04 05:41:15 +0700

    waysbeans

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -70,11 +70,10 @@ function Detail() {
 <div>
             <NavbarUser/>
             <Container  className="mt-5 pt-5">
-            {product?.map((item, index) => (
                 <Row className="mt-5">
                     <Col xs={12} md={5}>
                         <img 
-                        src={item?.image}
+                        src={product?.image}
                         style={{width: "80%" }}
                         className="img-fluid"
                         alt="transaction"
@@ -82,10 +81,10 @@ function Detail() {
                     </Col>
                     <Col xs={12} md={7} style={{backgroundColor:"white"}}>
                         <div>
-                            <h1 className='text-start brown fw-bold mb-3'>{item.name}</h1>
+                            <h1 className='text-start brown fw-bold mb-3'>{product.name}</h1>
                         </div>
                         <div className='mb-3'>
-                            <p className='text-start brown' style={{fontSize:"18px", color:"#974A4A"}}>{item?.stock}</p>
+                            <p className='text-start brown' style={{fontSize:"18px", color:"#974A4A"}}>{product?.stock}</p>
                         </div>
                         <div className='mb-5 mt-3'>
                             <Row>
@@ -94,14 +93,13 @@ function Detail() {
                             </Row>
                         </div>
                         <div className='d-flex' style={{justifyContent:"flex-end"}}>
-                            <h2 className='brown fw-bold fs-4 mt-3 mb-5'>Rp{item?.price}</h2>
+                            <h2 className='brown fw-bold fs-4 mt-3 mb-5'>Rp{product?.price}</h2>
                         </div>
                     <div className=''>
                         <Button className="btn btn-auth-brown fw-bold mb-4" style={{width:"100%"}} onClick={(e) => handleAddToCart.mutate(e)}>Add to Cart</Button>
                     </div>
                     </Col>
                 </Row>
-            ))}
             </Container>
         </div>
   )
