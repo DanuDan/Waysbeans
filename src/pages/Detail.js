85cacee2442636d703daab209d84d3e3080a6a77
DanuDan <ardanuaryhfz@gmail.com>
2022-10-04 07:21:25 +0700

    waysbeans

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -73,7 +73,7 @@ function Detail() {
                 <Row className="mt-5">
                     <Col xs={12} md={5}>
                         <img 
-                        src={product?.image?.subString(38)}
+                        src={product?.image.subString(38)}
                         style={{width: "80%" }}
                         className="img-fluid"
                         alt="transaction"
